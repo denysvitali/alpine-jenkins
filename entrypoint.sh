443a202b691c3c6816433ac6f217e92d4e86d81a
Denys Vitali <denys@denv.it>
2018-03-26 19:42:37 +0200

    Fix entrypoint variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker_gid = $(stat -c "%g" /var/run/docker.sock)
+docker_gid=$(stat -c "%g" /var/run/docker.sock)
 groupmod -g $docker_gid docker
 
 su - jenkins -c '/sbin/tini -- /usr/local/bin/jenkins.sh'
